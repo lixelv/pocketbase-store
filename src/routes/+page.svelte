a67a06c75d57b3a132520e391539b3d463209fe5
lixel_ <133605174+lixelv@users.noreply.github.com>
2024-11-11 16:50:05 +0300

    test error case

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import PocketBase from '$lib/index.js';
+	import PocketBase from 'pocketbase';
+	import { createCollectionStore } from '$lib';
 
 	type TestItem = {
 		id: string;
@@ -14,16 +15,13 @@
 
 	const pb = new PocketBase('https://pocketbase-control-hub.fly.dev/');
 	pb.autoCancellation(false);
-	// const testStore = pb.collection('test').store<TestItem>({ sort: '-name,created' }, data.test);
-	const testStore = pb.collection('test').storeItem<TestItem>('rhot1q2hgy03mkk');
+
+	const testStore = createCollectionStore<TestItem>(pb, 'test');
 
 	let value = '';
 </script>
 
-<input type="text" bind:value={$testStore.name} />
-<button on:click={async () => await testStore.send()}>Send</button>
-
-<!-- <form
+<form
 	on:submit|preventDefault={() => {
 		testStore.create({ name: value });
 		value = '';
@@ -32,8 +30,9 @@
 	<input type="text" bind:value />
 	<button type="submit">Add</button>
 </form>
+
 {#each $testStore as item}
-	{item.name}{item.id}
+	{item.name} - {item.id}
 	<button on:click={() => testStore.delete(item)}>Delete</button>
 	<br />
-{/each} -->
+{/each}
